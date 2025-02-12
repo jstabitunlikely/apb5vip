6aa06b16a047ee6269d886f1e1e3b8a0a602ccc9
Gergo Gacs <gacsgergo@gmail.com>
2025-02-12 18:38:49 +0800

    add a few more coverpoints

--- src/apb5vip/Apb5CoverageCollector.py
+++ src/apb5vip/Apb5CoverageCollector.py
@@ -4,7 +4,7 @@ from .Apb5Types import *
 from .Apb5Transfer import Apb5Transfer
 
 
-class Apb5CoverageCollector(uvm_subscriber):
+class Apb5TransferCoverage:
 
     cp_address = CoverPoint(
         name="transfer.address",
@@ -22,20 +22,38 @@ class Apb5CoverageCollector(uvm_subscriber):
         bins_labels=[d.name for d in Direction],
     )
 
+    cp_strobe = CoverPoint(
+        name="transfer.strobe",
+        xf=lambda x: x.strobe,
+        # TODO how to implement sampling conditions with cocotb-coverage?
+        bins=list(range(0x10)),
+    )
+
+    cross_address_x_direction = CoverCross(
+        "transfer.address_x_direction",
+        items=["transfer.address", "transfer.direction"],
+    )
+
     coverage = coverage_section(
         cp_address,
         cp_direction,
+        cp_strobe,
+        cross_address_x_direction,
     )
 
     @coverage
     def sample(self, transfer):
         pass
 
+
+class Apb5CoverageCollector(uvm_subscriber):
+
     def __init__(self, name="coverage_collector", parent=None):
         super().__init__(name, parent)
+        self.transfer_coverage = Apb5TransferCoverage()
 
     def write(self, transfer: Apb5Transfer):
-        self.sample(transfer)
+        self.transfer_coverage.sample(transfer)
 
     def report_phase(self):
         coverage_db.export_to_yaml()
